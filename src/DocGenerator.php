f94db82d631066ca6b7969c804918a54e21fba48
Jarred Stelfox <jarstelfox@mac.com>
2020-02-22 10:50:53 -0800

    DocGenerator: Get the ball rolling
    Now we are printing info about the parsed classes

--- src/DocGenerator.php
+++ src/DocGenerator.php
@@ -27,5 +27,45 @@ class DocGenerator {
    }
 
    public function generateDocs(): void {
+      foreach ($this->getIncludedClasses() as $classLike) {
+         $methods = $classLike->methods;
+         foreach ($methods as $methodLike) {
+            $doc = $this->getDocComment($methodLike->stmt_location);
+            var_dump([
+               'fullClassName' => $classLike->name,
+               'methodName' => $methodLike->cased_name,
+               'docBlockText' => $doc ? $doc->getText() : '',
+            ]);
+         }
+      }
+   }
+
+   /**
+    * @return ClassLikeStorage[]
+    */
+   private function getIncludedClasses(): array {
+      return array_filter($this->classMap, function ($classLike) {
+         return !$this->isClassExcluded($classLike);
+      });
+   }
+
+   private function isClassExcluded(ClassLikeStorage $classLike): bool {
+      $codeLocation = $classLike->namespace_name_location;
+      return $this->isBarePhp($codeLocation) ||
+             $this->isInVendor($codeLocation);
+   }
+
+   private function isInVendor(?CodeLocation $codeLocation): bool {
+      $filePath = $codeLocation ? $codeLocation->file_path : '';
+      $isVendor = (bool)strstr($filePath, 'vendor');
+      return $isVendor;
+   }
+
+   private function isBarePhp(?CodeLocation $codeLocation): bool {
+      return $codeLocation === null;
+   }
+
+   private function getDocComment(?CodeLocation $codeLocation): ?Doc {
+      return null;
    }
 }
